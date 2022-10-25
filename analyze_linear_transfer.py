8302b79c2e1702bc36abf561a7dd75534937ab68
Harel Cain <harel@lightricks.com>
2022-10-25 22:02:41 +0300

    Code refactoring ahead of upcoming elections.

--- analyze_linear_transfer.py
+++ analyze_linear_transfer.py
@@ -46,7 +46,7 @@ def adapt_df(df, parties, include_no_vote=False, ballot_number_field_name=None):
     return df
 
 def solve_transfer_coefficients(x_data, y_data, verbose):
-    M = cvx.Variable([x_data.shape[1], y_data.shape[1]])
+    M = cvx.Variable((x_data.shape[1], y_data.shape[1]))
     constraints = [0 <= M, M <= 1, cvx.sum(M, axis=1) == 1]
     objective = cvx.Minimize(cvx.norm((x_data @ M) - y_data, 'fro'))
     prob = cvx.Problem(objective, constraints)
@@ -64,8 +64,8 @@ def sankey(vote_movements, before_labels, after_labels, n_ballots):
     import time
     source, target = np.meshgrid(np.arange(0, len(before_labels)),
                                  np.arange(len(before_labels), len(before_labels) + len(after_labels)))
-    before_labels = [x + '_23' for x in before_labels]
-    after_labels = [x + '_24' for x in after_labels]
+    before_labels = [x + '_24' for x in before_labels]
+    after_labels = [x + '_25' for x in after_labels]
     source = source.flatten()
     target = target.flatten()
 
@@ -84,15 +84,19 @@ def sankey(vote_movements, before_labels, after_labels, n_ballots):
         ))])
 
 
-    fig.update_layout(title_text="""
-    ניתוח נדידת קולות בין הבחירות לכנסת העשרים וארבע לבחירות לכנסת העשרים וחמש על סמך ניתוח {} קלפיות בעלות מספר סידורי זהה שהופיעו שהשתתפו בשתיהן
-    <br>נוצר על ידי הראל קין, בשעה {}
-    """.format(n_ballots, time.strftime('%d.%m.%Y %H%:%M'), title_font_size=13, font_size=14))
+    fig.update_layout(title_text=f"""
+Analysis of vote transfer between the elections for the 24th and the 25th Knessets.
+<br>Based on analysis of {n_ballots} ballots whose serial number appeared in both. 
+<br>Created by Harel Cain on {time.strftime('%d.%m.%Y %H%:%M')}. All rights reserved.
+<br>Source code: https://github.com/harelc/elections-vote-transfer/
+<br>
+<br>""", title_font_size=16, font_size=14)
     fig.write_html("index.html")
     fig.show()
 
 
 if __name__ == '__main__':
+    method = "convex solver"
     ballot_previous = pd.read_csv('ballot23final.csv', encoding='iso8859_8')
     ballot_current = pd.read_csv('ballot24.csv', encoding='iso8859_8')
     parties_previous = 'פה מחל ודעם שס ל ג טב אמת'.split()
@@ -109,23 +113,25 @@ if __name__ == '__main__':
     ))
     values_previous = ballot_previous.loc[u.index].values
     values_current = ballot_current.loc[u.index].values
-    print(values_previous[:,:-1].sum(), values_current[:,:-1].sum())
 
-    #### method 1: closed-form solution with no non-negative constraint
-    # M = values_current.T @ values_previous @ np.linalg.pinv(values_previous.T @ values_previous)
+    if method == "closed form":
+        #### method 1: closed-form solution with no non-negative constraint
+        M = values_current.T @ values_previous @ np.linalg.pinv(values_previous.T @ values_previous)
 
-    ### method 2: non-negative least square solution
-    # M = np.zeros((values_current.shape[1], values_previous.shape[1]))
-    # for i in range(values_current.shape[1]):
-    #     sol, r2 = nnls(values_previous, values_current[:, i])
-    #     M[i,:] = sol
-    #     pred = values_previous @ sol
-    #     res = pred - values_current[:, i]
-    #     # print MSE, MAE, sum of error
-    #     # print(r2, np.mean(np.abs(res)), res.sum())
+    elif method == "nnls":
+        ### method 2: non-negative least square solution
+        M = np.zeros((values_current.shape[1], values_previous.shape[1]))
+        for i in range(values_current.shape[1]):
+            sol, r2 = nnls(values_previous, values_current[:, i])
+            M[i,:] = sol
+            pred = values_previous @ sol
+            res = pred - values_current[:, i]
+            # print MSE, MAE, sum of error
+            # print(r2, np.mean(np.abs(res)), res.sum())
 
-    ### method 3: use convex solver with constraints
-    M = solve_transfer_coefficients(values_previous, values_current, True).T
+    elif method == "convex solver":
+        ## method 3: use convex solver with constraints
+        M = solve_transfer_coefficients(values_previous, values_current, verbose=True).T
 
     y_bar = values_current.mean(axis=0)
     ss_tot = ((values_current - y_bar) ** 2).sum()
@@ -134,7 +140,6 @@ if __name__ == '__main__':
     print(M.sum(axis=0))
     print(M.sum(axis=1))
 
-    print(M.shape)
     vote_movements = M * ballot_previous.sum(axis=0).values
     print('Removing vote movements smaller than 5000')
     vote_movements[vote_movements < 5000] = 0.
